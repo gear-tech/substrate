eb464017e6d7f3274f7e1c49bf60c391dce16122
clearloop <26088946+clearloop@users.noreply.github.com>
2022-12-04 00:35:21 +0800

    Merge pull request #26 from gear-tech/cl/fix-logging
    chore(sc-tracing): fix the max_level implementation of Subscriber

--- client/tracing/src/logging/mod.rs
+++ client/tracing/src/logging/mod.rs
@@ -90,18 +90,6 @@ fn to_log_level_filter(level_filter: Option<LevelFilter>) -> log::LevelFilter {
 	}
 }
 
-/// Convert a [`log::LevelFilter`] to a `Option<LevelFilter>`.
-fn from_log_level_filter(level: log::LevelFilter) -> LevelFilter {
-    match level {
-        log::LevelFilter::Info => LevelFilter::INFO,
-        log::LevelFilter::Trace => LevelFilter::TRACE,
-        log::LevelFilter::Warn => LevelFilter::WARN,
-        log::LevelFilter::Error => LevelFilter::ERROR,
-        log::LevelFilter::Debug => LevelFilter::DEBUG,
-        log::LevelFilter::Off => LevelFilter::OFF,
-    }
-}
-
 /// Common implementation to get the subscriber.
 fn prepare_subscriber<N, E, F, W>(
 	directives: &str,
@@ -155,11 +143,15 @@ where
 				.expect("provided directive is valid"),
 		);
 
-	if let Ok(lvl) = std::env::var("RUST_LOG") {
-		if lvl != "" {
-			env_filter = parse_user_directives(env_filter, &lvl)?;
-		}
-	}
+    if let Ok(mut lvl) = std::env::var("RUST_LOG") {
+        if let Some(max_level) = max_level_override {
+            lvl = filter_directives(max_level, &lvl)
+        }
+
+		    if lvl != "" {
+            env_filter = parse_user_directives(env_filter, &lvl)?;
+		    }
+	  }
 
 	if directives != "" {
 		env_filter = parse_user_directives(env_filter, directives)?;
@@ -194,6 +186,7 @@ where
 		enable_color,
 		dup_to_stdout: !atty::is(atty::Stream::Stderr) && atty::is(atty::Stream::Stdout),
 	};
+
 	let builder = FmtSubscriber::builder().with_env_filter(env_filter);
 
 	let builder = builder.with_span_events(format::FmtSpan::NONE);
@@ -203,8 +196,6 @@ where
 	let builder = builder.event_format(event_format);
 
 	let builder = builder_hook(builder);
-
-  	let builder = builder.with_max_level(from_log_level_filter(max_level));
 
 	let subscriber = builder.finish().with(PrefixLayer);
 
